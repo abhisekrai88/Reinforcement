5b1668e078e726917db67182218b7feb4bbfff0e
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-05 21:35:37 +0800

    setting more states and preventdefault

--- todoapp/src/components/Form.js
+++ todoapp/src/components/Form.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 
-const Form = () =>{
+const Form = ({ setInputText }) =>{
     //JS code and functions can be written below
     const inputTextHandler = (e) => {
         console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        
     };
     return(
         <form>
             <input onChange={inputTextHandler} type="text" className="todo-input"/>
-            <button className="todo-button" type="submit">
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
