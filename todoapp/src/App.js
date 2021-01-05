dab78b10f0dd29d062697bafb2c2619abaec06d4
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-06 01:13:36 +0800

    styling

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -46,7 +46,7 @@ function App() {
     if(localStorage.getItem("todos") === null){
       localStorage.setItem("todos", JSON.stringify([]));
     } else{
-      let todoLocal= localStorage.getItem("todos", JSON.stringify(todos))
+      let todoLocal= JSON.parse(localStorage.getItem("todos"));
       setTodos(todoLocal);
     }
   }
