fea406a7cb99b2764920a7c475d2b2f48863de37
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-06 00:02:02 +0800

    adding filter for tasks

--- todoapp/src/components/Form.js
+++ todoapp/src/components/Form.js
@@ -14,6 +14,9 @@ const Form = ({ setInputText, todos, setTodos, inputText }) =>{
         ]);
         setInputText("");
     };
+    const statusHandler = (e) => {
+
+    }
     return(
         <form>
             <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input"/>
@@ -21,7 +24,7 @@ const Form = ({ setInputText, todos, setTodos, inputText }) =>{
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
-                <select name="todos" className="filter-todo">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="outstanding">Outstanding</option>
