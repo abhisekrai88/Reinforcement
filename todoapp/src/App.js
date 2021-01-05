401aa2ad827021645ac48392df041b309ab92c28
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-05 20:41:14 +0800

    added components and data for forms.js

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>My Todo List</h1>
+      </header>
     </div>
   );
 }
